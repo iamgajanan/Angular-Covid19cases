a44fc2a172eabb938c5e30ba9636f5d9321305dd
Gajanan Shinde <gajanan.shinde@creaxt.com>
2020-07-21 18:11:20 +0530

    new Api

--- src/app/covid19.service.ts
+++ src/app/covid19.service.ts
@@ -7,12 +7,16 @@ import { Observable } from 'rxjs';
 })
 export class Covid19Service {
 private getalldata="https://api.covid19api.com/summary"
-private getStateurl="https://api.covidindiatracker.com/state_data.json"
+private getDistricturl="https://api.covid19india.org/state_district_wise.json"
+private getStateurl="https://covid-india-cases.herokuapp.com/states"
   constructor(private http:HttpClient) { }
   getall():Observable<any[]>{
     return this.http.get<any[]>(this.getalldata)
    }
    getState():Observable<any[]>{
     return this.http.get<any[]>(this.getStateurl)
+   }
+   getDistrict():Observable<any[]>{
+    return this.http.get<any[]>(this.getDistricturl)
    }
 }
